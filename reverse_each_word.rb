9021d00ca51afd5cfcdf5fbc1f46b4c900046a16
Roy  Flores <royfloresnyc@gmail.com>
2020-07-21 18:36:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,12 +2,12 @@ def reverse_each_word(sentence)
   result = sentence.split(" ").collect do |word|
     word = word.split('')
     index = word.length - 1 
-    reversed_word = ""
+    reversed_word = []
     while index >= 0 do
-      reversed_word + word[index]
+      reversed_word << word[index]
       index -= 1
     end 
-    reversed_word
+    reversed_word.join('')
   end
   result.join(" ")
 end 
